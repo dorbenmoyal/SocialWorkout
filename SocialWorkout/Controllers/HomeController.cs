67ef2bd123ddcc518dbebed226025fc943e9cd86
TzachBen <tzach_ben@hotmail.com>
2018-05-17 12:34:14 +0300

    asdfasdfasdfasdf

--- SocialWorkout/Controllers/HomeController.cs
+++ SocialWorkout/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SocialWorkout.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your applicdation description page.";
 
             return View();
         }
